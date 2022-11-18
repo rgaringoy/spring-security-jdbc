966395a72ed3cfff52c3a5a259af8e596bae7ea4
roy <roeder.aringoy@gmail.com>
2022-11-18 10:47:45 +0800

    Initial commit

---
+++ src/main/java/dev/roy/jdbcuser/JdbcUserApplication.java
@@ -0,0 +1,13 @@
+package dev.roy.jdbcuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/roy/jdbcuser/JdbcUserApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.roy.jdbcuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
